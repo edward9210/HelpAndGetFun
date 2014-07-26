9777d3c2593970be858ccffa7366ab607b310761
edward9210 <edward@Edward.local>
2014-07-26 18:15:00 +0800

    fix make friend bug

--- phpsrc/makefriends.php
+++ phpsrc/makefriends.php
@@ -5,11 +5,15 @@
   
   if ($name1!="" && $name2!=""){
     include 'mysqlconnect.php';
-    $query="insert into is_friend(user1,user2) value('$name1','$name2')";
-    $result=mysql_query($query,$id);
-    /*$query2="insert into is_friend(user1,user2) value('$name2','$name1')";
-    $result=mysql_query($query2,$id);*/
-    print("success");
+    $rownum=mysql_num_rows(mysql_query("select * from user where username='$name2'", $id));
+    if($rownum>0) 
+    {
+      $query="insert into is_friend(user1,user2) value('$name1','$name2')";
+      $result=mysql_query($query,$id);
+      /*$query2="insert into is_friend(user1,user2) value('$name2','$name1')";
+      $result=mysql_query($query2,$id);*/
+      print("success");
+    }
     mysql_close($id);
   }
 ?>
