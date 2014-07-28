d8fc1905c682165aee7b7575982114a74504def4
edward9210 <edward@Edward.local>
2014-07-28 19:32:24 +0800

    delete phpsrc.zip and fix mysqlconnect.php

--- phpsrc/mysqlconnect.php
+++ phpsrc/mysqlconnect.php
@@ -1,8 +1,5 @@
 <?php
-  $host="SAE_MYSQL_HOST_M";
-  $dbuser="SAE_MYSQL_USER";
-  $dbpass="SAE_MYSQL_PASS";
-  $id=mysql_connect($host,$dbuser,$dbpass);
+  $id=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
   $ok=mysql_select_db("app_helpandgetfun",$id);
   mysql_query("SET NAMES 'gb2312'",$id);
 ?>
