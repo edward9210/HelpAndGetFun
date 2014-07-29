d0a9598fb8661b93856fd9016501347a563c7edb
edward9210 <879667863@qq.com>
2014-07-29 14:24:48 +0800

    change upload dir
    Signed-off-by: edward9210 <879667863@qq.com>

--- phpsrc/receiveImg.php
+++ phpsrc/receiveImg.php
@@ -1,5 +1,5 @@
 ﻿<?php  
-    $target_path  = SAE_TMP_PATH;//接收文件目录  
+	$target_path  = 'saestor://headimg/';//接收文件目录  
     $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);  
     if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {  
        print ("The file ".  basename( $_FILES['uploadedfile']['name']). " has been uploaded");  
